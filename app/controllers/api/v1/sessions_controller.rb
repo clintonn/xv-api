3084b40ad241dfbd8efdf9136c62a94fb34ceb11
Clinton Nguyen <clinton.ngyn@gmail.com>
2017-04-05 18:39:52 -0400

    Add session endpoints

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,7 +3,18 @@ require 'logger'
 class Api::V1::SessionsController < Api::V1::ApplicationController
 
   def create
-
+    @user = User.find_by(email: params[:email])
+    if @user
+      @user.authenticate(params[:password]) ? (
+        jwt = Auth.issue({id: @user.id})
+        render json: {
+          jwt: jwt,
+          user: { id: @user.id, firstName: @user.first_name, lastName: @user.last_name, email: @user.email, username: @user.username, resumes: @user.resumes }
+        }
+      ) : (render status: 500)
+    else
+      render status: 401
+    end
   end
 
   def destroy
